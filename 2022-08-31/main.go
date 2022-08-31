9370d16f25891c894b73111bf1e0082814519b75
fenno <714326675@qq.com>
2022-08-31 17:06:26 +0800

    type-demo2

--- 2022-08-31/main.go
+++ 2022-08-31/main.go
@@ -1,1 +1,18 @@
-package _022_08_31
+package main
+
+import (
+	"fmt"
+)
+
+type aaa func() string
+func main()  {
+
+	var h aaa = hello;
+	//var string h = hello();
+	fmt.Println(h())
+}
+
+
+func hello() string {
+	return "hello"
+}
